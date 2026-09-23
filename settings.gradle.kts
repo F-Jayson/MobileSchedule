b5e42df18e308c68cbb98fc56d64273ece957768
F-Jayson <65397565+F-Jayson@users.noreply.github.com>
2026-09-23 22:05:40 +0800

    build: 依赖仓库改成腾讯云镜像

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,13 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
     }
 }
 
